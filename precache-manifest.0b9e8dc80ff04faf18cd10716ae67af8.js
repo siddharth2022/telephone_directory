ae4e224b5421d67fdc9b1488b6c43cfc8efdce8a
Your Name <siddhrathyadav1999@gmail.com>
2019-03-14 10:20:42 +0530

    Updates

---
+++ precache-manifest.0b9e8dc80ff04faf18cd10716ae67af8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/telephone_directory/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "74a40551dd41c3aee6db",
+    "url": "/telephone_directory/static/js/runtime~main.362d4c49.js"
+  },
+  {
+    "revision": "07318f843006b36327e4",
+    "url": "/telephone_directory/static/js/main.b089b445.chunk.js"
+  },
+  {
+    "revision": "c6800ba3f219d7204b52",
+    "url": "/telephone_directory/static/js/2.a657bb28.chunk.js"
+  },
+  {
+    "revision": "07318f843006b36327e4",
+    "url": "/telephone_directory/static/css/main.b7be3d04.chunk.css"
+  },
+  {
+    "revision": "0ad606226ad3e4b85e8a102e3ca74def",
+    "url": "/telephone_directory/index.html"
+  }
+];
